e9884a584f2856a772898e8f6b3d56df337197e1
Justin J <132813834+FustinCS@users.noreply.github.com>
2023-07-29 22:55:31 -0700

    Add getComputerChoice() Function

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,1 +1,14 @@
-console.log("Hello World");
+function getComputerChoice() {
+    let randomNum = Math.floor(Math.random() * 3) + 1;
+    if (randomNum === 1) {
+        return "Rock";
+    }
+    else if (randomNum === 2) {
+        return "Paper";
+    }
+    else if (randomNum === 3) {
+        return "Scissors";
+    }
+}
+
+console.log(getComputerChoice());
